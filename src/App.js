3efd92dce6e9b603b94f5721f10c884e28755de0
Gary T Almes <garytalmes@gmail.com>
2023-06-05 12:41:23 -0400

    Updated link code.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import { useEffect, useState } from "react"
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
   const [ currPage, setCurrPage ] = useState("home")
+  const [ prefix, setPrefix ] = useState("")
+
+  useEffect(() => {
+    if( process.env.NODE_ENV === "production" ){
+      setPrefix("/react-github-norouter")
+    }
+  }, [process.env.NODE_ENV])
 
   useEffect(() => {
     const path = window.location.href.split(window.location.host)[1]
     console.log(path)
+    setCurrPage(path)
   }, [window.location.href])
 
   return (
     <div className="App">
-      <p>Here is the main app page</p>
+      <Header prefix={prefix} />
+
+      { currPage === `${prefix}/` && <Home /> }
+      { currPage === `${prefix}/about` && <About /> }
+      { currPage === `${prefix}/contact` && <Contact /> }
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,13 @@
 
-
-
-export default function Header(){
+export default function Header({prefix}){
 
   return (
     <div>
       <p>Links:</p>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href={`${prefix}/`}>Home</a></li>
+        <li><a href={`${prefix}/about`}>About</a></li>
+        <li><a href={`${prefix}/contact`}>Contact</a></li>
       </ul>
     </div>
   )
